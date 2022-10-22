b535e3f63a10d6b61f77c8af27eb688d93cd6934
shmykovandrey <shmykovandrey88@gmail.com>
2022-10-22 11:32:14 +0300

    feat: add src and test pug includes

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,6 @@ const port = 3000;
 
 app.use(express.static("dist"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
