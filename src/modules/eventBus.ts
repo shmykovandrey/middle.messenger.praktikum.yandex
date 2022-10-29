eb2588c401bce2fd48375ae66b52cd918723c277
shmykovandrey <shmykovandrey88@gmail.com>
2022-10-29 15:46:26 +0300

    feat: add ts, esling airbnb, add lint command, add eventBus like module

---
+++ src/modules/eventBus.ts
@@ -0,0 +1,40 @@
+type Listeners = {
+  [index: string]: string[];
+};
+
+interface IEventBus {
+  listneners: Listeners;
+  on: (event: string, callback: () => void) => void;
+}
+
+export default class EventBus implements IEventBus {
+  listneners = {};
+
+  constructor() {
+    this.listneners = [];
+  }
+
+  on(event, callback) {
+    if (!this.listneners[event]) {
+      this.listneners[event] = [];
+    }
+    this.listneners[event].push(callback);
+  }
+
+  emit(event, ...args) {
+    if (!this.listneners[event]) {
+      throw new Error(`Нет такого эвента ${event}`);
+    }
+    // console.log(event);
+    // console.log(...args);
+    this.listneners[event].forEach((listener) => listener(...args));
+  }
+
+  off(event, callback) {
+    if (!this.listneners[event]) {
+      throw new Error(`Нет такого эвента ${event}`);
+    }
+
+    this.listneners[event].filter((listener) => listener !== callback);
+  }
+}
