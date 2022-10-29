97bf5cdc63025571690ae266fdadf24a06b492f3
shmykovandrey <shmykovandrey88@gmail.com>
2022-10-29 16:59:18 +0300

    fix: eslintconfig

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import EventBus from "./modules/eventBus";
+import EventBus from './modules/eventBus';
 
 const eventBus = new EventBus();
 
 const fn1 = (a = 1, b = 2) => {
-  console.log("fn1");
+  console.log('fn1');
   console.log(a + b);
 };
 
 const fn2 = (a = 3, b = 4) => {
-  console.log("fn2");
+  console.log('fn2');
   console.log(a * b);
 };
 
 try {
-  eventBus.on("sum", fn1);
-  eventBus.emit("sum", 3, 4);
-  eventBus.on("sum", fn2);
-  eventBus.emit("sum", 3, 4);
+  eventBus.on('sum', fn1);
+  eventBus.emit('sum', 3, 4);
+  eventBus.on('sum', fn2);
+  eventBus.emit('sum', 3, 4);
 } catch (error) {
   console.error(error);
 }
