004d3fe2a2b20c0112843f1a9ae545a42e6855ca
Feomatar Feomatar <shmykovandrey88@gmail.com>
2022-11-05 19:16:48 +0300

    feat: add index page

--- src2/index.ts
+++ src2/index.ts
@@ -4,36 +4,43 @@ import Nav from './layouts/nav/nav';
 import Input from './components/input/input';
 import Form from './components/form/form';
 
-const buttonTest = new Button('div', {
-  className: 'test',
-  text: 'Классаная какая вышла кнопка',
-});
-
-const nav = new Nav('nav', {
-  button1: buttonTest,
-  button2: new Button('div', {
-    className: 'test2',
-    text: 'Классаная вышла кнопка',
-  }),
-  button3: new Button('div', {
-    className: 'test3',
-    text: 'Классаная вышла кнопка опять',
-  }),
-});
-
-const inputLogin = new Input('div', {
-  inputType: 'text',
-  placeholder: 'login',
-  name: 'login',
-});
-
-// const loginForm = new Form('div', {
-//   inputs: [1, 2, 3, 4, 5],
+// const buttonTest = new Button('div', {
+//   className: 'test',
+//   text: 'Классаная какая вышла кнопка',
 // });
 
-window.buttonTest = buttonTest;
-window.nav = nav;
-window.input = inputLogin;
-// window.loginForm = loginForm;
+// const nav = new Nav('nav', {
+//   link1: 'aaaa',
+//   link2: new Button('div', {
+//     className: 'test2',
+//     text: 'Классаная вышла кнопка',
+//   }),
+//   button3: new Button('div', {
+//     className: 'test3',
+//     text: 'Классаная вышла кнопка опять',
+//   }),
+// });
 
+// const inputLogin = new Input('div', {
+//   inputType: 'text',
+//   placeholder: 'login',
+//   name: 'login',
+// });
+
+// // const loginForm = new Form('div', {
+// //   inputs: [1, 2, 3, 4, 5],
+// // });
+
+// window.buttonTest = buttonTest;
+// window.nav = nav;
+// window.input = inputLogin;
+// // window.loginForm = loginForm;
+
+const nav = new Nav('div'){
+  render(){
+    return super.compile(tpl)
+  }
+}
+
+console.log(nav)
 renderDom('.app', nav);
--- src2/layouts/nav/template.ts
+++ src2/layouts/nav/template.ts
@@ -1,7 +1,19 @@
-const tmp = `<ul>
-<li>!{button1}<li>
-<li>!{button2}<li>
-<li>!{button3}<li>
-</ul>`;
+const tmp = `
+main
+    h1 Ссылки на все страницы.
+    nav 
+    ul 
+        li
+        a(href='./pages/login/login.html').link Страница Login
+        li
+        a(href='./pages/registration/registration.html').link Страница Регистрации
+        li
+        a(href='./pages/main/main.html').link Страница чата.
+        li
+        a(href='./pages/profile/profile.html').link Страница профиля
+        li
+        a(href='./pages/page404/page404.html').link Страница ошибки 404
+        li
+        a(href='./pages/page500/page500.html').link Страница 500`;
 
 export default tmp;
